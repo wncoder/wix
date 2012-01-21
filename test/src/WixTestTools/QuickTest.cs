a69831ac5e410dda7e7d990540498892e3df7286
Rob Mensching <robm@robmensching.com>
2012-01-20 23:59:00 -0800

    Add missing license in headers.
    --HG--
    branch : wix36

--- test/src/WixTestTools/QuickTest.cs
+++ test/src/WixTestTools/QuickTest.cs
@@ -1,6 +1,14 @@
 ﻿//-----------------------------------------------------------------------
 // <copyright file="QuickTest.cs" company="Microsoft">
 //     Copyright (c) Microsoft Corporation.  All rights reserved.
+//    
+//    The use and distribution terms for this software are covered by the
+//    Common Public License 1.0 (http://opensource.org/licenses/cpl1.0.php)
+//    which can be found in the file CPL.TXT at the root of this distribution.
+//    By using this software in any fashion, you are agreeing to be bound by
+//    the terms of this license.
+//    
+//    You must not remove this notice, or any other, from this software.
 // </copyright>
 // <summary>
 //     This class provides a quick and simple way of running a test
