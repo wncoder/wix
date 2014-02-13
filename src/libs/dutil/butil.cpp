wncoder/wix

commit b646e096742b1fdb608d34bb3fddf3ee76006ef5

Style cleanup

src/libs/dutil/butil.cpp
@@ -21,8 +21,8 @@ const LPCWSTR BUNDLE_REGISTRATION_REGISTRY_BUNDLE_UPGRADE_CODE = L"BundleUpgrade
 const LPCWSTR BUNDLE_REGISTRATION_REGISTRY_BUNDLE_PROVIDER_KEY = L"BundleProviderKey";
 
 // Forward declarations.
-HRESULT OpenBundleKey(
-    __in LPCWSTR szBundleId,
+static HRESULT OpenBundleKey(
+    __in LPCWSTR wzBundleId,
     __in BUNDLE_INSTALL_CONTEXT context, 
     __inout HKEY *key);
 
@@ -30,13 +30,13 @@ HRESULT OpenBundleKey(
 BundleGetBundleInfo - Queries the bundle installation metadata for a given property
 
 RETURNS:
-    ERROR_INVALID_PARAMETER
+    E_INVALIDARG
         An invalid parameter was passed to the function.
-    ERROR_UNKNOWN_PRODUCT   
+    HRESULT_FROM_WIN32(ERROR_UNKNOWN_PRODUCT)
         The bundle is not installed
-    ERROR_UNKNOWN_PROPERTY 
+    HRESULT_FROM_WIN32(ERROR_UNKNOWN_PROPERTY)
         The property is unrecognized
-    ERROR_MORE_DATA
+    HRESULT_FROM_WIN32(ERROR_MORE_DATA)
         A buffer is too small to hold the requested data.
     E_NOTIMPL:
         Tried to read a bundle attribute for a type which has not been implemented
@@ -44,121 +44,76 @@ RETURNS:
     All other returns are unexpected returns from other dutil methods.
 ********************************************************************/
 extern "C" HRESULT DAPI BundleGetBundleInfo(
-  __in LPCWSTR   szBundleId,                             // Bundle code
-  __in LPCWSTR   szAttribute,                            // attribute name
-  __out_ecount_opt(*pcchValueBuf) LPWSTR lpValueBuf,     // returned value, NULL if not desired
-  __inout_opt                     LPDWORD pcchValueBuf   // in/out buffer character count
+  __in LPCWSTR wzBundleId,
+  __in LPCWSTR wzAttribute,
+  __out_ecount_opt(*pcchValueBuf) LPWSTR lpValueBuf,
+  __inout_opt LPDWORD pcchValueBuf
   )
 {
-    Assert(szBundleId && szAttribute);
+    Assert(wzBundleId && wzAttribute);
 
-    HRESULT hr = ERROR_UNKNOWN_PROPERTY;
+    HRESULT hr = S_OK;
     BUNDLE_INSTALL_CONTEXT context = BUNDLE_INSTALL_CONTEXT_MACHINE;
-    LPWSTR szValue = NULL;
-    HKEY bundleKey = NULL;
+    LPWSTR sczValue = NULL;
+    HKEY hkBundle = NULL;
     DWORD cchSource = 0;
     DWORD dwType = 0;
     DWORD dwValue = 0;
 
-    if ((lpValueBuf && !pcchValueBuf) || !szBundleId || !szAttribute)
+    if ((lpValueBuf && !pcchValueBuf) || !wzBundleId || !wzAttribute)
     {
-        ExitOnFailure(hr = ERROR_INVALID_PARAMETER, "An invalid parameter was passed to the function.");
+        ExitOnFailure(hr = E_INVALIDARG, "An invalid parameter was passed to the function.");
     }
 
-    if ((hr = OpenBundleKey(szBundleId, 
-                                   context = BUNDLE_INSTALL_CONTEXT_MACHINE,
-                                   &bundleKey)) != ERROR_SUCCESS &&
-        (hr = OpenBundleKey(szBundleId, 
-                                   context = BUNDLE_INSTALL_CONTEXT_USER,
-                                   &bundleKey)) != ERROR_SUCCESS)
+    if (FAILED(hr = OpenBundleKey(wzBundleId, context = BUNDLE_INSTALL_CONTEXT_MACHINE, &hkBundle)) &&
+        FAILED(hr = OpenBundleKey(wzBundleId, context = BUNDLE_INSTALL_CONTEXT_USER, &hkBundle)))
     {
-        ExitOnFailure(hr = ERROR_UNKNOWN_PRODUCT, "Failed to locate bundle uninstall key path.");
+        ExitOnFailure(E_FILENOTFOUND == hr ? HRESULT_FROM_WIN32(ERROR_UNKNOWN_PRODUCT) : hr, "Failed to locate bundle uninstall key path.");
     }
 
     // If the bundle doesn't have the property defined, return ERROR_UNKNOWN_PROPERTY
-    hr = RegGetType(bundleKey, szAttribute, &dwType);
-    if (FAILED(hr))
-    {
-        ExitOnFailure(hr = ERROR_UNKNOWN_PROPERTY, "Failed to locate bundle property.");
-    }
+    hr = RegGetType(hkBundle, wzAttribute, &dwType);
+    ExitOnFailure(E_FILENOTFOUND == hr ? HRESULT_FROM_WIN32(ERROR_UNKNOWN_PROPERTY) : hr, "Failed to locate bundle property.");
 
-    switch(dwType)
+    switch (dwType)
     {
         case REG_SZ:
-            hr = RegReadString(bundleKey, szAttribute, &szValue);
+            hr = RegReadString(hkBundle, wzAttribute, &sczValue);
             ExitOnFailure(hr, "Failed to read string property.");
             break;
         case REG_DWORD:
-            hr = RegReadNumber(bundleKey, szAttribute, &dwValue);
+            hr = RegReadNumber(hkBundle, wzAttribute, &dwValue);
             ExitOnFailure(hr, "Failed to read dword property.");
 
-            hr = StrAllocFormatted(&szValue, L"%d", dwValue);
+            hr = StrAllocFormatted(&sczValue, L"%d", dwValue);
             ExitOnFailure(hr, "Failed to format dword property as string.");
             break;
-
-        // TODO: read REG_MULTI_SZ, but as it's a multi value we would either need to concat it as a delimited string or provide an additional indexed input.
-        //case REG_MULTI_SZ:
-
         default:
             ExitOnFailure1(hr = E_NOTIMPL, "Reading bundle info of type 0x%x not implemented.", dwType);
 
     }
 
-    hr = ::StringCchLengthW(szValue, STRSAFE_MAX_CCH, reinterpret_cast<UINT_PTR*>(&cchSource));
+    hr = ::StringCchLengthW(sczValue, STRSAFE_MAX_CCH, reinterpret_cast<UINT_PTR*>(&cchSource));
     ExitOnFailure(hr, "Failed to calculate length of string");
 
-    if(lpValueBuf)
+    if (lpValueBuf)
     {
         // cchSource is the length of the string not including the terminating null character
         if (*pcchValueBuf <= cchSource)
         {
-            *pcchValueBuf = cchSource++;
-            ExitOnFailure(hr = ERROR_MORE_DATA, "A buffer is too small to hold the requested data.");
+            *pcchValueBuf = ++cchSource;
+            ExitOnFailure(hr = HRESULT_FROM_WIN32(ERROR_MORE_DATA), "A buffer is too small to hold the requested data.");
         }
 
-        hr = ::StringCchCatNExW(lpValueBuf, *pcchValueBuf, szValue, cchSource, NULL, NULL, STRSAFE_FILL_BEHIND_NULL);
+        hr = ::StringCchCatNExW(lpValueBuf, *pcchValueBuf, sczValue, cchSource, NULL, NULL, STRSAFE_FILL_BEHIND_NULL);
         ExitOnFailure(hr, "Failed to copy the property value to the output buffer.");
         
         *pcchValueBuf = cchSource++;        
     }
-    
+
 LExit:
-    ReleaseRegKey(bundleKey);
-    ReleaseStr(szValue);
-
-    return hr;
-}
-
-/********************************************************************
-OpenBundleKey - Opens the bundle uninstallation key for a given bundle
-
-NOTE: caller is responsible for closing key
-********************************************************************/
-HRESULT OpenBundleKey(
-    __in LPCWSTR szBundleId,
-    __in BUNDLE_INSTALL_CONTEXT context, 
-    __inout HKEY *key)
-{
-    Assert(key && szBundleId);
-    AssertSz(NULL == *key, "*key should be null");
-
-    HRESULT hr = S_OK;
-    HKEY root = HKEY_LOCAL_MACHINE;
-    REGSAM access = KEY_READ;
-    LPWSTR keypath = NULL;
-
-    if (context == BUNDLE_INSTALL_CONTEXT_USER)
-    {
-        root = HKEY_CURRENT_USER;
-    }
-
-    hr = StrAllocFormatted(&keypath, L"%ls\\%ls", BUNDLE_REGISTRATION_REGISTRY_UNINSTALL_KEY, szBundleId);
-    ExitOnFailure(hr, "Failed to allocate bundle uninstall key path.");
-    
-    hr = RegOpen(root, keypath, access, key);
-    ExitOnFailure(hr, "Failed to open bundle uninstall key path.");
-LExit:
-    ReleaseStr(keypath);
+    ReleaseRegKey(hkBundle);
+    ReleaseStr(sczValue);
 
     return hr;
 }
@@ -169,26 +124,25 @@ BundleEnumRelatedBundle - Queries the bundle installation metadata for installs 
 NOTE: lpBundleIdBuff is a buffer to receive the bundle GUID. This buffer must be 39 characters long. 
         The first 38 characters are for the GUID, and the last character is for the terminating null character.
 RETURNS:
-    ERROR_INVALID_PARAMETER
+    E_INVALIDARG
         An invalid parameter was passed to the function.
 
     All other returns are unexpected returns from other dutil methods.
 ********************************************************************/
 HRESULT DAPI BundleEnumRelatedBundle(
-  __in     LPCWSTR lpUpgradeCode,
-  __in     BUNDLE_INSTALL_CONTEXT context,
-  __inout  PDWORD pdwStartIndex,
-  __out_ecount(MAX_GUID_CHARS+1)  LPWSTR lpBundleIdBuf
+  __in LPCWSTR wzUpgradeCode,
+  __in BUNDLE_INSTALL_CONTEXT context,
+  __inout PDWORD pdwStartIndex,
+  __out_ecount(MAX_GUID_CHARS+1) LPWSTR lpBundleIdBuf
     )
 {
     HRESULT hr = S_OK;
-    HKEY root = HKEY_LOCAL_MACHINE;
-    HKEY uninstallKey = NULL;
-    HKEY bundleKey = NULL;
-    REGSAM access = KEY_READ;
-    LPWSTR uninstallSubKey = NULL;
-    LPWSTR uninstallSubKeyPath = NULL;
-    LPWSTR szValue = NULL;
+    HKEY hkRoot = BUNDLE_INSTALL_CONTEXT_USER == context ? HKEY_CURRENT_USER : HKEY_LOCAL_MACHINE;
+    HKEY hkUninstall = NULL;
+    HKEY hkBundle = NULL;
+    LPWSTR sczUninstallSubKey = NULL;
+    LPWSTR sczUninstallSubKeyPath = NULL;
+    LPWSTR sczValue = NULL;
     DWORD dwType = 0;
 
     LPWSTR* rgsczBundleUpgradeCodes = NULL;
@@ -198,58 +152,53 @@ HRESULT DAPI BundleEnumRelatedBundle(
     LPWSTR szProviderKey = NULL;
     DWORD cchProviderKey = 0;
 
-    if ((NULL == lpUpgradeCode)||(NULL == lpBundleIdBuf)||(NULL == pdwStartIndex))
+    if (!wzUpgradeCode || !lpBundleIdBuf || !pdwStartIndex)
     {
-        ExitOnFailure(hr = ERROR_INVALID_PARAMETER, "An invalid parameter was passed to the function.");
+        ExitOnFailure(hr = E_INVALIDARG, "An invalid parameter was passed to the function.");
     }
 
-    if (context == BUNDLE_INSTALL_CONTEXT_USER)
-    {
-        root = HKEY_CURRENT_USER;
-    }
-
-    hr = RegOpen(root, BUNDLE_REGISTRATION_REGISTRY_UNINSTALL_KEY,access, &uninstallKey);
+    hr = RegOpen(hkRoot, BUNDLE_REGISTRATION_REGISTRY_UNINSTALL_KEY, KEY_READ, &hkUninstall);
     ExitOnFailure(hr, "Failed to open bundle uninstall key path.");
 
-    for(DWORD dwIndex = *pdwStartIndex; !fUpgradeCodeFound; dwIndex++)
+    for (DWORD dwIndex = *pdwStartIndex; !fUpgradeCodeFound; dwIndex++)
     {
-        hr = RegKeyEnum(uninstallKey, dwIndex, &uninstallSubKey);
+        hr = RegKeyEnum(hkUninstall, dwIndex, &sczUninstallSubKey);
         ExitOnFailure(hr, "Failed to enumerate bundle uninstall key path.");
 
-        hr = StrAllocFormatted(&uninstallSubKeyPath, L"%ls\\%ls", BUNDLE_REGISTRATION_REGISTRY_UNINSTALL_KEY, uninstallSubKey);
+        hr = StrAllocFormatted(&sczUninstallSubKeyPath, L"%ls\\%ls", BUNDLE_REGISTRATION_REGISTRY_UNINSTALL_KEY, sczUninstallSubKey);
         ExitOnFailure(hr, "Failed to allocate bundle uninstall key path.");
         
-        hr = RegOpen(root, uninstallSubKeyPath, access, &bundleKey);
+        hr = RegOpen(hkRoot, sczUninstallSubKeyPath, KEY_READ, &hkBundle);
         ExitOnFailure(hr, "Failed to open uninstall key path.");
 
         // If it's a bundle, it should have a BundleUpgradeCode value of type REG_SZ (old) or REG_MULTI_SZ
-        hr = RegGetType(bundleKey, BUNDLE_REGISTRATION_REGISTRY_BUNDLE_UPGRADE_CODE, &dwType);
+        hr = RegGetType(hkBundle, BUNDLE_REGISTRATION_REGISTRY_BUNDLE_UPGRADE_CODE, &dwType);
         if (FAILED(hr))
         {
-            ReleaseRegKey(bundleKey);
-            ReleaseNullStr(uninstallSubKey);
-            ReleaseNullStr(uninstallSubKeyPath);
+            ReleaseRegKey(hkBundle);
+            ReleaseNullStr(sczUninstallSubKey);
+            ReleaseNullStr(sczUninstallSubKeyPath);
             // Not a bundle
             continue;
         }
 
-        switch(dwType)
+        switch (dwType)
         {
             case REG_SZ:
-                hr = RegReadString(bundleKey, BUNDLE_REGISTRATION_REGISTRY_BUNDLE_UPGRADE_CODE, &szValue);
+                hr = RegReadString(hkBundle, BUNDLE_REGISTRATION_REGISTRY_BUNDLE_UPGRADE_CODE, &sczValue);
                 ExitOnFailure(hr, "Failed to read BundleUpgradeCode string property.");
-                if (CSTR_EQUAL == ::CompareStringW(LOCALE_INVARIANT, NORM_IGNORECASE, szValue, -1, lpUpgradeCode, -1))
+                if (CSTR_EQUAL == ::CompareStringW(LOCALE_INVARIANT, NORM_IGNORECASE, sczValue, -1, wzUpgradeCode, -1))
                 {
                     *pdwStartIndex = dwIndex;
                     fUpgradeCodeFound = TRUE;
                     break;
                 }
 
-                ReleaseNullStr(szValue);
+                ReleaseNullStr(sczValue);
 
                 break;
             case REG_MULTI_SZ:
-                hr = RegReadStringArray(bundleKey, BUNDLE_REGISTRATION_REGISTRY_BUNDLE_UPGRADE_CODE, &rgsczBundleUpgradeCodes, &cBundleUpgradeCodes);
+                hr = RegReadStringArray(hkBundle, BUNDLE_REGISTRATION_REGISTRY_BUNDLE_UPGRADE_CODE, &rgsczBundleUpgradeCodes, &cBundleUpgradeCodes);
                 ExitOnFailure(hr, "Failed to read BundleUpgradeCode  multi-string property.");
 
                 for (DWORD i = 0; i < cBundleUpgradeCodes; i++)
@@ -257,7 +206,7 @@ HRESULT DAPI BundleEnumRelatedBundle(
                     LPWSTR wzUpgradeCode = rgsczBundleUpgradeCodes[i];
                     if (wzUpgradeCode && *wzUpgradeCode)
                     {
-                        if (CSTR_EQUAL == ::CompareStringW(LOCALE_INVARIANT, NORM_IGNORECASE, wzUpgradeCode, -1, lpUpgradeCode, -1))
+                        if (CSTR_EQUAL == ::CompareStringW(LOCALE_INVARIANT, NORM_IGNORECASE, wzUpgradeCode, -1, wzUpgradeCode, -1))
                         {
                             *pdwStartIndex = dwIndex;
                             fUpgradeCodeFound = TRUE;
@@ -274,49 +223,66 @@ HRESULT DAPI BundleEnumRelatedBundle(
 
         }
 
-        if(fUpgradeCodeFound)
+        if (fUpgradeCodeFound)
         {
-            hr = RegReadString(bundleKey, BUNDLE_REGISTRATION_REGISTRY_BUNDLE_PROVIDER_KEY, &szProviderKey );
+            hr = RegReadString(hkBundle, BUNDLE_REGISTRATION_REGISTRY_BUNDLE_PROVIDER_KEY, &szProviderKey );
             ExitOnFailure(hr, "Failed to read the bundle provider key.");
 
             hr = ::StringCchLengthW(szProviderKey, STRSAFE_MAX_CCH, reinterpret_cast<UINT_PTR*>(&cchProviderKey));
             ExitOnFailure(hr, "Failed to calculate length of string");
 
-            if(lpBundleIdBuf)
+            if (lpBundleIdBuf)
             {
-                // cchSource is the lenght of the string not including the terminating null character
-                //if (*pcchValueBuf <= cchProviderKey)
-                //{
-                //	*pcchValueBuf = cchSource++;
-                //	ExitOnFailure(hr = ERROR_MORE_DATA, "A buffer is too small to hold the requested data.");
-                //}
-
-
-                hr = ::StringCchCatNExW(lpBundleIdBuf, 39, szProviderKey, cchProviderKey, NULL, NULL, STRSAFE_FILL_BEHIND_NULL);
+                hr = ::StringCchCatNExW(lpBundleIdBuf, MAX_GUID_CHARS + 1, szProviderKey, cchProviderKey, NULL, NULL, STRSAFE_FILL_BEHIND_NULL);
                 ExitOnFailure(hr, "Failed to copy the property value to the output buffer.");
-        
-                //*pcchValueBuf = cchSource++;        
             }
 
             break;
         }
 
         // Cleanup before next iteration
-        ReleaseRegKey(bundleKey);
-        ReleaseNullStr(uninstallSubKey);
-        ReleaseNullStr(uninstallSubKeyPath);
+        ReleaseRegKey(hkBundle);
+        ReleaseNullStr(sczUninstallSubKey);
+        ReleaseNullStr(sczUninstallSubKeyPath);
     }
-
 
 LExit:
     ReleaseStr(szProviderKey);
-    ReleaseStr(szValue);
-    ReleaseStr(uninstallSubKey);
-    ReleaseStr(uninstallSubKeyPath);
-    ReleaseRegKey(bundleKey);
-    ReleaseRegKey(uninstallKey);
+    ReleaseStr(sczValue);
+    ReleaseStr(sczUninstallSubKey);
+    ReleaseStr(sczUninstallSubKeyPath);
+    ReleaseRegKey(hkBundle);
+    ReleaseRegKey(hkUninstall);
     ReleaseStrArray(rgsczBundleUpgradeCodes, cBundleUpgradeCodes);
 
+    return hr;
+}
+
+/********************************************************************
+OpenBundleKey - Opens the bundle uninstallation key for a given bundle
+
+NOTE: caller is responsible for closing key
+********************************************************************/
+HRESULT OpenBundleKey(
+    __in LPCWSTR wzBundleId,
+    __in BUNDLE_INSTALL_CONTEXT context, 
+    __inout HKEY *key)
+{
+    Assert(key && wzBundleId);
+    AssertSz(NULL == *key, "*key should be null");
+
+    HRESULT hr = S_OK;
+    HKEY hkRoot = BUNDLE_INSTALL_CONTEXT_USER == context ? HKEY_CURRENT_USER : HKEY_LOCAL_MACHINE;
+    LPWSTR sczKeypath = NULL;
+
+    hr = StrAllocFormatted(&sczKeypath, L"%ls\\%ls", BUNDLE_REGISTRATION_REGISTRY_UNINSTALL_KEY, wzBundleId);
+    ExitOnFailure(hr, "Failed to allocate bundle uninstall key path.");
+    
+    hr = RegOpen(hkRoot, sczKeypath, KEY_READ, key);
+    ExitOnFailure(hr, "Failed to open bundle uninstall key path.");
+
+LExit:
+    ReleaseStr(sczKeypath);
 
     return hr;
 }

src/libs/dutil/dutil.vcxproj
@@ -60,7 +60,7 @@
     <ClCompile Include="aclutil.cpp" />
     <ClCompile Include="apuputil.cpp" />
     <ClCompile Include="atomutil.cpp" />
-	<ClCompile Include="butil.cpp" />
+    <ClCompile Include="butil.cpp" />
     <ClCompile Include="buffutil.cpp" />
     <ClCompile Include="cabcutil.cpp" />
     <ClCompile Include="cabutil.cpp" />
@@ -115,7 +115,7 @@
     <ClInclude Include="inc\apuputil.h" />
     <ClInclude Include="inc\atomutil.h" />
     <ClInclude Include="inc\buffutil.h" />
-	<ClInclude Include="inc\butil.h" />
+    <ClInclude Include="inc\butil.h" />
     <ClInclude Include="inc\cabcutil.h" />
     <ClInclude Include="inc\cabutil.h" />
     <ClInclude Include="inc\certutil.h" />

commit 4e48014cc89afd90d60947a7a45afe812fed5351

Fix for bug introduced in commit b646e096742b1fdb608d34bb3fddf3ee76006ef5 for feature #4193.

src/libs/dutil/butil.cpp
@@ -203,10 +203,10 @@ HRESULT DAPI BundleEnumRelatedBundle(
 
                 for (DWORD i = 0; i < cBundleUpgradeCodes; i++)
                 {
-                    LPWSTR wzUpgradeCode = rgsczBundleUpgradeCodes[i];
-                    if (wzUpgradeCode && *wzUpgradeCode)
+                    LPWSTR wzBundleUpgradeCode = rgsczBundleUpgradeCodes[i];
+                    if (wzBundleUpgradeCode && *wzBundleUpgradeCode)
                     {
-                        if (CSTR_EQUAL == ::CompareStringW(LOCALE_INVARIANT, NORM_IGNORECASE, wzUpgradeCode, -1, wzUpgradeCode, -1))
+                        if (CSTR_EQUAL == ::CompareStringW(LOCALE_INVARIANT, NORM_IGNORECASE, wzBundleUpgradeCode, -1, wzUpgradeCode, -1))
                         {
                             *pdwStartIndex = dwIndex;
                             fUpgradeCodeFound = TRUE;